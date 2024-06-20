89ffb53a36bb838007910391239c48eb77cad6ab
luongthanhquy <luongthanhquyv@gmail.com>
2024-06-20 17:10:09 +0700

    change test

--- wordpress_data/wp-content/plugins/woocommerce/tests/bootstrap.php
+++ wordpress_data/wp-content/plugins/woocommerce/tests/bootstrap.php
@@ -1,17 +1,12 @@
 <?php
-// tests/bootstrap.php
+// tests/YourPluginTest.php
 
-$_tests_dir = getenv('WP_TESTS_DIR');
-if (!$_tests_dir) {
-    $_tests_dir = rtrim(sys_get_temp_dir(), '/\\') . '/wordpress-tests-lib';
+class YourPluginTest extends WP_UnitTestCase {
+    function test_sample() {
+        $this->assertTrue(true);
+    }
+
+    function test_plugin_loaded() {
+        $this->assertTrue(is_plugin_active('your-plugin/your-plugin.php'));
+    }
 }
-
-require_once $_tests_dir . '/includes/functions.php';
-
-tests_add_filter('muplugins_loaded', '_manually_load_plugin');
-
-function _manually_load_plugin() {
-    require dirname(__FILE__) . '/../wp-content/plugins/your-plugin/your-plugin.php';
-}
-
-require $_tests_dir . '/includes/bootstrap.php';
